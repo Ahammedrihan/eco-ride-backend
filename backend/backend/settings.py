b2aebbc0faef19f9ebc9f955a7b34200f5bceefe
Ahammed <ahammedrihancm@gmail.com>
2023-12-30 10:31:17 +0530

    path changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -206,9 +206,9 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
   
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    # "https://www.eco-ride.online"
+    # "http://localhost:3000",
+    # "http://127.0.0.1:3000",
+    "https://www.eco-ride.online"
 ]
 
 
